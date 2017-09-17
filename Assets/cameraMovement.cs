a7d85ec3af0321adc5f3135f2c086c22457fee60
Paul Hellsten <phellsten@student.unimelb.edu.au>
2017-09-18 00:12:49 +1000

    Basic movement and jumping
    Uses ray tracing to detect if on the ground, but is still a little buggy when on the side of a ledge

---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,26 @@
+﻿// Paul Hellsten - 758077
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+	public GameObject player;
+	private Vector2 mouseLook;
+	private Vector2 deltaMousePos;
+	public float sens = 2.0f;
+
+	void Update () {
+		// Change new direction based on mouse axis.
+		deltaMousePos.x = Mathf.Lerp(deltaMousePos.x, Input.GetAxisRaw("Mouse X") * sens, 1f);
+		deltaMousePos.y = Mathf.Lerp(deltaMousePos.y, Input.GetAxisRaw("Mouse Y") * sens, 1f);
+
+		mouseLook += deltaMousePos;
+
+		// Clamp camera between -90 and 90 degrees.
+		mouseLook.y = Mathf.Clamp (mouseLook.y, -90f, 90f);
+
+		// Apply rotation
+		transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+		player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+	}
+}
