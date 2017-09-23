a35a3cba55dc86b2e738540ad3f8c8c4c06ceeea
jkaloger <jkaloger@users.noreply.github.com>
2017-09-23 15:00:34 +1000

    scene, mats, particle effect, invoker

---
+++ Assets/explosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Explode() {
+		ParticleSystem particle = GetComponent<ParticleSystem>;
+		particle.Play ();
+		Destroy(gameObject, particle.duration);
+	}
+}
