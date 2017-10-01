496d911f93687464754ecc552b36ab7258562934
Cameron Dempsey <cameron.dempsey@gmail.com>
2017-10-01 17:12:15 +1100

    Edit enemy movement to automatically follow the player

--- Assets/Prefabs/Enemies/Monster1/MonsterController.cs
+++ Assets/Prefabs/Enemies/Monster1/MonsterController.cs
@@ -7,60 +7,78 @@ public class MonsterController : MonoBehaviour {
 
     private NavMeshAgent nav;
     private Animator anim;
-    private IEnumerator arriveChecker;
-    private bool moving = false;
-    private bool running = false;
+    private Transform playerTransform;
+    private IEnumerator moveChecker;
+    //private bool moving = false;
+    //private bool running = false;
+
+    public float moveCheckDelay = 0.1f;
+
 
     void Awake () {
         nav = GetComponent<NavMeshAgent>();
         anim = GetComponentInChildren<Animator>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        moveChecker = CheckPlayerPosition();
+        StartCoroutine(moveChecker);
+        anim.SetBool("Walking", true);
 	}
 	
-	void Update () {
-        if (!moving && running)
-        {
-            Debug.Log("Stop coroutine");
-            StopCoroutine(arriveChecker);
-            running = false;
-        }
-        // Set destination
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
+	//void Update () {
+        //if (!moving && running)
+        //{
+        //    Debug.Log("Stop coroutine");
+        //    StopCoroutine(arriveChecker);
+        //    running = false;
+        //}
 
-            float cameraY = Camera.main.transform.position.y;
-            Vector2 mouseScreenPos = Input.mousePosition;
-            Vector3 screenPosWithZDist = (Vector3)mouseScreenPos + (Vector3.forward * cameraY);
-            Vector3 fireToWorldPos = Camera.main.ScreenToWorldPoint(screenPosWithZDist);
-            Debug.Log("New Pos: " + fireToWorldPos);
-            nav.SetDestination(fireToWorldPos);
-            anim.SetBool("Walking",true);
-            moving = true;
-            arriveChecker = CheckForArrival();
-            StartCoroutine(arriveChecker);
-            running = true;
-        }
+        // Set destination
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //
+        //    float cameraY = Camera.main.transform.position.y;
+        //    Vector2 mouseScreenPos = Input.mousePosition;
+        //    Vector3 screenPosWithZDist = (Vector3)mouseScreenPos + (Vector3.forward * cameraY);
+        //    Vector3 fireToWorldPos = Camera.main.ScreenToWorldPoint(screenPosWithZDist);
+        //    Debug.Log("New Pos: " + fireToWorldPos);
+        //    nav.SetDestination(fireToWorldPos);
+        //    anim.SetBool("Walking",true);
+        //    moving = true;
+        //    arriveChecker = CheckForArrival();
+        //    StartCoroutine(arriveChecker);
+        //    running = true;
+        //}
         
+    //}
+
+    // Stop when encountered the player
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StopCoroutine(moveChecker);
+            anim.SetBool("Walking", false);
+        }
     }
-    
-    // Check if at arrival point every 0.1 seconds.
-    IEnumerator CheckForArrival()
+
+    // Restart movement if player moves away
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(moveChecker);
+            anim.SetBool("Walking", true);
+        }
+    }
+
+    // Reassigns movement target to player location every 0.1 seconds.
+    IEnumerator CheckPlayerPosition()
     {     
-        for (;;)
+        while (true)
         {
             Debug.Log("Run coroutine");
-            if (!nav.pathPending)
-            {
-                if (nav.remainingDistance <= nav.stoppingDistance)
-                {
-                    if (!nav.hasPath || nav.velocity.sqrMagnitude == 0f)
-                    {
-                        Debug.Log("Arrived");
-                        anim.SetBool("Walking", false);
-                        moving = false;
-                    }
-                }
-            }
-            yield return new WaitForSeconds(0.1f);
+            nav.SetDestination(playerTransform.position);
+            yield return new WaitForSeconds(moveCheckDelay);
         }
     }
 }
