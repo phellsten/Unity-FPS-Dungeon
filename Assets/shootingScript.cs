ae3e8d38d9a1a61974e951250d4f1547a2011964
Paul Hellsten <phellsten@student.unimelb.edu.au>
2017-10-10 20:41:37 +1100

    reloading 'animation' and proper reloading

--- Assets/shootingScript.cs
+++ Assets/shootingScript.cs
@@ -12,6 +12,7 @@ public class shootingScript : MonoBehaviour {
 
 	private Vector3 aimPos = new Vector3 (0f, -0.288f, 0.682f);
 	private Vector3 normalPos = new Vector3 (0.499f, -0.414f, 0.806f);
+    private Vector3 reloadPos = new Vector3(0.499f, -0.857f, 0.806f);
 
 	private float gunMoveSpeed = 5f;
 	private float muzzleOffset = 1f;
@@ -23,11 +24,15 @@ public class shootingScript : MonoBehaviour {
 
     private float yNormalHeight = -0.414f;
     private Quaternion recoilRot = new Quaternion(0f, 0.9659f, -0.2588f, 0f);
+    private Quaternion reloadRot = new Quaternion(0.0333f, 0.922f, 0.3851f, 0f);
 
     private Camera mainCam;
 
     private Text ammoDisplay;
     private int ammoCount;
+
+    private bool reloading = false;
+    private float reloadTimer = 2f;
 
     private void Start()
     {
@@ -44,28 +49,50 @@ public class shootingScript : MonoBehaviour {
 			return;
 		}
 
-        // Reloading
-        if(Input.GetKey(KeyCode.R))
+        if(reloading)
         {
-            //toDo : Reloading animation
-            ammoCount += 1;
-            this.ammoDisplay.text = "Ammo: " + ammoCount;
+            reloadTimer -= Time.deltaTime;
+
+            // Move gun down to reload
+            this.transform.localRotation = Quaternion.Lerp(this.transform.localRotation, reloadRot, 10f * Time.deltaTime);
+            this.transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, reloadPos, 10f * Time.deltaTime);
+
+            // Zoom out if we were aiming before we pressed reload
+            if (mainCam.fieldOfView <= normFov)
+            {
+                mainCam.fieldOfView += fovMoveSpeed * Time.deltaTime;
+            }
+
+            if (reloadTimer <= 0f)
+            {
+                reloading = false;
+                reloadTimer = 2f;
+                ammoCount = 8;
+                this.ammoDisplay.text = "Ammo: " + ammoCount;
+            }
+        }
+
+
+        // Reloading
+        if(Input.GetKeyDown(KeyCode.R) && !reloading)
+        {
+            reloading = true;
         }
         
-        if(this.transform.localPosition.y > yNormalHeight)
+        if(this.transform.localPosition.y > yNormalHeight && !reloading)
         {
             // Return gun to normal height after recoil
             this.transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, new Vector3(this.transform.localPosition.x, yNormalHeight, this.transform.localPosition.z), 5f * Time.deltaTime);
         }
 
-        if (this.transform.localRotation != new Quaternion(0f, 1f, 0f, 0f))
+        if (this.transform.localRotation != new Quaternion(0f, 1f, 0f, 0f) && !reloading)
         {
             // Return gun to normal rotation after recoil
             this.transform.localRotation = Quaternion.Lerp(this.transform.localRotation, new Quaternion(0f,1f,0f,0f), 10f * Time.deltaTime);
         }
 
         // Shooting
-		if (Input.GetMouseButtonDown(0) && ammoCount > 0) {
+        if (Input.GetMouseButtonDown(0) && ammoCount > 0 && !reloading) {
             ammoCount -= 1;
             this.ammoDisplay.text = "Ammo: " + ammoCount;
 
@@ -100,7 +127,7 @@ public class shootingScript : MonoBehaviour {
 		}
 
 		// Holding right click, move gun to aim down sights.
-		if (Input.GetMouseButton (1)) {
+		if (Input.GetMouseButton (1) && !reloading) {
 			this.transform.localPosition = Vector3.MoveTowards (this.transform.localPosition, aimPos, gunMoveSpeed * Time.deltaTime);
 			cursor.GetComponentInChildren<Canvas> ().enabled = false;
 
@@ -110,7 +137,7 @@ public class shootingScript : MonoBehaviour {
         }
 
 		// Right click not pressed, keep gun in normal position.
-		if (!Input.GetMouseButton (1)) {
+		if (!Input.GetMouseButton (1) && !reloading) {
 			this.transform.localPosition = Vector3.MoveTowards (this.transform.localPosition, normalPos, gunMoveSpeed * Time.deltaTime);
 			cursor.GetComponentInChildren<Canvas> ().enabled = true;
 
